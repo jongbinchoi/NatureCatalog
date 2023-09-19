88f52d8a30527671e365417902177f7fe27d6071
Kim Jaeho <57672969+nero453@users.noreply.github.com>
2023-09-20 00:37:04 +0900

    create Menubar

--- naturecatalog/src/App.vue
+++ naturecatalog/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <nav id = 'MenuBar'>
+    <img alt="logo1" src="./assets/image.png" marign="0" pading="0" width="180"><br>
+    <div id="MenuBar-text">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      login
+    </div>
   </nav>
+  <div style="padding-top: 120px;"></div>
   <router-view/>
+  </div>
 </template>
 
 <style>
@@ -15,16 +22,41 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+body {
+  margin: 0px auto;
+  padding: 0px;
 }
 
-nav a {
+#MenuBar {
+  /*메뉴바 상단으로 고정*/
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+
+  margin: 0 auto;
+  padding: 0 auto;
+  height: 80px;
+  width: 100%;
+
+  background-color: #ffffff;
+}
+#MenuBar-text {
+  background-color: #046325;
+  position: absolute;
+
+  height: 40px;
+  width: 100%;
+  font-size: 20px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #000000;
+  text-align: center;
+  text-decoration: none;
+  margin: 0px auto;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+  line-height: 40px;
 }
 </style>
